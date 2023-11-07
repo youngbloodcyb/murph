2d3e449cbb3ed26babc7ea0f093e816b0f89e651
Cameron Youngblood <cameron.youngblood@gmail.com>
2023-11-06 21:48:23 -0700

    feat: configure tremor

--- app/page.tsx
+++ app/page.tsx
@@ -1,26 +1,54 @@
 import MurphForm from "@/components/form";
+import { Card, LineChart, Title } from "@tremor/react";
 import prisma from "@/lib/prisma";
 import Murph from "@/components/murph";
 
 export default async function Home() {
-  const data = await prisma.murph.findMany();
+  const data = await prisma.murph.findMany({
+    orderBy: {
+      date: "desc",
+    },
+  });
+
+  const chartData = data.reverse().map((item) => {
+    return {
+      date: item.date.toDateString(),
+      pullups: item.pullups,
+      pushups: item.pushups,
+      squats: item.squats,
+    };
+  });
+
   return (
-    <main className="flex min-h-screen flex-col p-8 lg:p-24">
+    <main className="min-h-screen flex p-8 lg:p-16 gap-4">
       <MurphForm />
-      {data.map((murph) => (
-        <div key={murph.id} className="w-full">
-          <Murph
-            date={murph.date}
-            firstMileMin={murph.firstMileMin}
-            firstMileSec={murph.firstMileSec}
-            secondMileMin={murph.secondMileMin}
-            secondMileSec={murph.secondMileSec}
-            pullups={murph.pullups}
-            pushups={murph.pushups}
-            squats={murph.squats}
-          />
-        </div>
-      ))}
+      <div className="w-1/2">
+        {data.map((murph) => (
+          <div key={murph.id} className="w-full">
+            <Murph
+              date={murph.date}
+              firstMileMin={murph.firstMileMin}
+              firstMileSec={murph.firstMileSec}
+              secondMileMin={murph.secondMileMin}
+              secondMileSec={murph.secondMileSec}
+              pullups={murph.pullups}
+              pushups={murph.pushups}
+              squats={murph.squats}
+            />
+          </div>
+        ))}
+      </div>
+      <Card className="w-1/2 rounded-lg">
+        <Title>Reps</Title>
+        <LineChart
+          className="mt-6"
+          data={chartData}
+          index="date"
+          categories={["pullups", "pushups", "squats"]}
+          colors={["indigo", "cyan", "emerald"]}
+          yAxisWidth={40}
+        />
+      </Card>
     </main>
   );
 }
--- components/murph.tsx
+++ components/murph.tsx
@@ -11,48 +11,48 @@ export default function Murph({
   squats,
 }: any) {
   return (
-    <div className="shadow-sm w-full p-4 border border-slate-200 rounded-lg mt-8">
+    <div className="shadow-sm w-full p-4 border border-slate-200 rounded-lg mb-4">
       <div className="flex gap-4">
-        <div>
-          <ActivitySquare className="w-8 h-8" />
+        <div className="p-1">
+          <ActivitySquare className="w-10 h-10 text-slate-400" />
         </div>
         <div className="">
           <div>
             <p className="inline-flex items-center gap-2">
-              <span className="text-xs items-baseline text-blue-500">
+              <span className="text-xs items-baseline text-slate-500">
                 Mile times:
               </span>
-              <span>
+              <span className="text-sm">
                 {firstMileMin}:{firstMileSec.toString().padStart(2, "0")}
               </span>
               <MoveRight className="inline w-4" />
-              <span>
+              <span className="text-sm">
                 {secondMileMin}:{secondMileSec}
               </span>
             </p>
           </div>
           <div>
             <p className="inline-flex items-center gap-2">
-              <span className="text-xs items-baseline text-blue-500">
+              <span className="text-xs items-baseline text-slate-500">
                 Pull-ups:
               </span>
-              {pullups}
+              <span className="text-sm">{pullups}</span>
             </p>
           </div>
           <div>
             <p className="inline-flex items-center gap-2">
-              <span className="text-xs items-baseline text-blue-500">
+              <span className="text-xs items-baseline text-slate-500">
                 Push-ups:
               </span>
-              {pushups}
+              <span className="text-sm">{pushups}</span>
             </p>
           </div>
           <div>
             <p className="inline-flex items-center gap-2">
-              <span className="text-xs items-baseline text-blue-500">
+              <span className="text-xs items-baseline text-slate-500">
                 Squats:
               </span>
-              {squats}
+              <span className="text-sm">{squats}</span>
             </p>
           </div>
           <div className="inline-flex items-center gap-1">
